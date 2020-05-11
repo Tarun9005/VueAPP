52e63892de5d6a264033647f6db6cb161f12f67f
Tarun9005 <shekhertarun@gmail.com>
2020-05-11 13:26:00 +0530

    hot module replacement

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+<h2>The count is awesome :yes it is: {{count}}</h2>
+
+<p>
+<button @click="count+=1">Increment</button>
+</p>
+</div>
+</template>
+
+<script>
+export default{
+	data(){
+	return {count:0};
+	}
+}
+</script>
